59ec836f60d28c8a9829426c5499f19da754ec22
StacyUshakova <106428633+StacyUshakova@users.noreply.github.com>
2023-05-12 07:35:04 +0300

    Add files via upload

---
+++ HM_4_SQL.sql
@@ -0,0 +1,48 @@
+USE semimar_4;
+
+/* 1. Подсчитать общее количество лайков, которые получили пользователи
+младше 12 лет. */
+
+SELECT COUNT(*) AS total_likes
+FROM likes
+WHERE user_id IN(
+	SELECT profiles.user_id
+	FROM profiles
+	WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, curdate()) < 12);
+
+SELECT COUNT(*) AS total_likes
+FROM likes
+JOIN profiles ON profiles.user_id = likes.user_id
+WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, curdate()) < 12;
+
+/* 2. Определить кто больше поставил лайков (всего): мужчины или женщины. */ 
+SELECT profiles.gender, COUNT(*)
+FROM likes
+JOIN profiles ON profiles.user_id = likes.user_id
+GROUP BY profiles.gender;
+
+/* 3. Вывести всех пользователей, которые не отправляли сообщения. */
+SELECT *
+FROM users
+WHERE id NOT IN(SELECT DISTINCT from_user_id FROM messages);
+
+/* 4. Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех написал
+ему сообщений. */
+-- Возьмем пользователя с id 1 и найдем его друзей 
+
+SELECT  
+	CONCAT(firstname, ' ', lastname) AS users_name, 
+    COUNT(from_user_id) AS total_messages
+FROM messages
+JOIN users ON users.id = messages.from_user_id
+WHERE to_user_id = 1
+	AND from_user_id IN (SELECT -- выведим id друзей пользователя с id 1
+							CONCAT(IF(initiator_user_id = 1, '', initiator_user_id), 
+								   IF(target_user_id = 1, '', target_user_id))
+						FROM friend_requests
+						WHERE updated_at IS NOT NULL
+							AND status = 'approved'
+							AND (initiator_user_id = 1 OR target_user_id = 1))
+GROUP BY from_user_id
+ORDER BY 2 DESC
+LIMIT 1;
